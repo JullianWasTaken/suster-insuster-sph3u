ba050b1d3daafb33a6e45187d837e757918719fb
Jullian <83075638+JullianWasTaken@users.noreply.github.com>
2022-01-31 18:44:07 -0500

    added manual keys for override LGTM

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    pump.start(Pump.LEFT, 100)
+})
+input.onButtonPressed(Button.B, function () {
+    pump.stop(Pump.LEFT)
+})
+basic.showString("SPH3U RST | Will Melvin Jullian", 80)
+basic.forever(function () {
+    if (soil.ifMoisture(soil.SoilPin.P0, Mlevel.VERY_WET)) {
+        pump.stop(Pump.LEFT)
+    }
+})
 basic.forever(function () {
     if (touch.getTouch(touch.TouchPin.P0)) {
         pump.start(Pump.LEFT, 100)
         basic.pause(5000)
-        pump.stop(Pump.LEFT)
-    } else {
-        pump.stop(Pump.LEFT)
-    }
-    if (soil.ifMoisture(soil.SoilPin.P0, Mlevel.VERY_WET)) {
         pump.stop(Pump.LEFT)
     }
 })
